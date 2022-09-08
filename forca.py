5d648092e782d600529366fe808624e02ab6ea67
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2022-09-08 12:51:43 -0300

    Update forca.py
    Implementando for para buscar letras

--- forca.py
+++ forca.py
@@ -3,6 +3,22 @@ def jogar():
     print("**     BEM-VINDO AO JOGO DA FORCA!      **")
     print("******************************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print("Encontrei a letra {} na posição {}.".format(letra, index))
+            index += 1
+        print("Jogando...")
+
     print("Fim do jogo!")
 
 
