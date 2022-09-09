e2cbda28976b49dd3ddf324b71c2d29fed26b7c7
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2022-09-09 10:25:09 -0300

    Update forca.py
    Melhorando a legibilidade utilizando funções

--- forca.py
+++ forca.py
@@ -2,23 +2,9 @@ import random
 
 
 def jogar():
-    print("******************************************")
-    print("**     BEM-VINDO AO JOGO DA FORCA!      **")
-    print("******************************************")
-
-    arquivo = open("palavras.txt", "r")
-    palavras = []
-
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
-
-    arquivo.close()
-
-    numero = random.randrange(0, len(palavras))
-    palavra_secreta = palavras[numero].upper()
-
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    abertura()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -50,5 +36,31 @@ def jogar():
     print("Fim do jogo!")
 
 
+def abertura():
+    print("******************************************")
+    print("**     BEM-VINDO AO JOGO DA FORCA!      **")
+    print("******************************************")
+
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+
 if __name__ == "__main__":
     jogar()
+
