affadce861c4338fecb01363bcb81d94d80bc521
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2022-09-09 14:34:59 -0300

    Update forca.py
    Refatorando o código com funções

--- forca.py
+++ forca.py
@@ -2,26 +2,21 @@ import random
 
 
 def jogar():
+
     abertura()
     palavra_secreta = carrega_palavra_secreta()
     letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
 
     enforcou = False
     acertou = False
     erros = 0
 
-    print(letras_acertadas)
-
     while not enforcou and not acertou:
-        chute = input("Qual a letra? ")
-        chute = chute.strip().upper()
+        chute = pede_chute()
 
         if chute in palavra_secreta:
-            index = 0
-            for letra in palavra_secreta:
-                if chute == letra:
-                    letras_acertadas[index] = letra
-                index += 1
+            marca_chute_correto(chute, letras_acertadas, palavra_secreta)
         else:
             erros += 1
 
@@ -30,16 +25,23 @@ def jogar():
         print(letras_acertadas)
 
     if acertou:
-        print("Você ganhou!")
+        imprime_mensagem_vencedor()
     else:
-        print("Você perdeu.")
-    print("Fim do jogo!")
+        imprime_mensagem_perdedor()
 
 
 def abertura():
     print("******************************************")
     print("**     BEM-VINDO AO JOGO DA FORCA!      **")
     print("******************************************")
+
+
+def imprime_mensagem_vencedor():
+    print("Você venceu o jogo!")
+
+
+def imprime_mensagem_perdedor():
+    print("Você perdeu o jogo.")
 
 
 def carrega_palavra_secreta():
@@ -55,6 +57,19 @@ def carrega_palavra_secreta():
     numero = random.randrange(0, len(palavras))
     palavra_secreta = palavras[numero].upper()
     return palavra_secreta
+
+
+def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if chute == letra:
+            letras_acertadas[index] = letra
+        index += 1
+
+def pede_chute():
+    chute = input("Qual a letra? ")
+    chute = chute.strip().upper()
+    return chute
 
 
 def inicializa_letras_acertadas(palavra):
